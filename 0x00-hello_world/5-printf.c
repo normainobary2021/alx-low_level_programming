088e05baf100454236b69a1ce80b2c4abf21ffb4
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2023-07-13 16:59:07 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main program entry
+ * Return: return 0 (Success)
+ **/
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

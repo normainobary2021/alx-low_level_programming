1c9c0de489848e2667dbacc9e57d63f1a5329fbe
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2023-07-13 16:42:11 -0700

    add changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
-int main(void){
+#include <stdio.h>
+/**
+ * main - the main function
+ * return - always 0
+ * /
+int main(void)
+{
 	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }

e5174b07231c9a844c5930bbd10cb5eac63f0583
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2023-07-13 16:49:33 -0700

    return value comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - the main function
- * return - always 0
+ * return - the return value is 0
  **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * return - the return is 0 as below
+	 **/
 	return (0);
 }

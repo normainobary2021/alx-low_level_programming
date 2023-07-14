d8bf96df3b2127adfb9661abcf1898fa85c2a515
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2023-07-13 17:02:30 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main program entry
- * Return: return 0 (Success)
+ * main - Entry point
+ * Return: always 0 (Success)
  **/
-int main(0)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

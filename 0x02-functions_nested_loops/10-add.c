24f267eef977dd2a1fb48dd82f91cd2e3128cb01
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2023-07-18 17:12:29 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @m1: The first number
+ * @m2: The second number
+ * Return: Integer value
+ **/
+
+int add(int m1, int m2)
+{
+	return (m1 + m2);
+}
